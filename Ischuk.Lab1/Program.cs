46f2c5e2058c75df51e4408c138db2bb1c8c9556
AlexandrIschuk <ischukar2004@gmail.com>
2022-11-29 02:16:37 +0400

    Добавьте файлы проекта.

---
+++ Ischuk.Lab1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Ischuk.Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Чему равно значение функции: F = sin^2(log5(b)/sqrt(cos(2a))");
+            double a = 0;
+            bool isNumber = false;
+            while (!isNumber)
+            {
+                Console.Write("Введите число A: ");
+                isNumber = double.TryParse(Console.ReadLine(), out a);
+                if (!isNumber)
+                {
+                    Console.WriteLine("Вы ввели не число!");
+                }
+            }
+            double b = 0;
+            bool isNumberB = false;
+            while (!isNumberB)
+            {
+                Console.Write("Введите число B: ");
+                isNumberB = double.TryParse(Console.ReadLine(), out b);
+                if (!isNumberB)
+                {
+                    Console.WriteLine("Вы ввели не число!");
+                }
+            }
+            double s1 = Math.Log(b, 5);
+            double s2 = Math.Sqrt(Math.Cos(2 * a));
+            double result = Math.Pow(Math.Sin(s1 / s2), 2);
+            Console.WriteLine("Ответ: " + result);
+            Console.ReadKey();
+        }
+    }
+}
